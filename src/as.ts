04b5833a354321c09a3156cb1a896af9af0209d6
Dehemart <martins.denis@gmail.com>
2022-06-17 14:18:06 -0300

    add test structure

---
+++ src/as.ts
@@ -0,0 +1,11 @@
+
+export class Opas {
+  /**
+       * @param  {number} x
+       * @param  {number} y
+       * @return number
+       */
+  static opas( x: number, y: number ): number {
+    return x + y;
+  }
+}
---
+++ test/as.spec.ts
@@ -0,0 +1,5 @@
+import { Opas } from "../src/as"
+
+test( 'sum two numbers positives', () => {
+  expect( Opas.opas( 3, 3 ) ).toBe( 6 );
+} );
